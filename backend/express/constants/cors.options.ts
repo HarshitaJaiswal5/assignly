3422c2c7ca1160f4ae4832555b9eaea9db3ae5ac
HarshitaJaiswal5 <harshitajaiswal1704005@gmail.com>
2026-08-18 17:48:05 +0530

    backend(express): setup of express server

---
+++ backend/express/constants/cors.options.ts
@@ -0,0 +1,17 @@
+
+export const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, status: boolean) => void
+  ) => {
+    if (origin === process.env.CLIENT_ORIGIN || !origin ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
